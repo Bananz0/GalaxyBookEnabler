d4353451e3217cdbeb55d98ad79b391dbf590608
Glen Muthoka <glenmuthokaq@gmail.com>
2023-12-30 21:53:19 +0300

    Bat FIle

--- QS.bat
+++ QS.bat
@@ -1,12 +1,6 @@
 @echo off
-
-set "key=HKLM\HARDWARE\DESCRIPTION\System\BIOS"
-set "values=BaseBoardManufacturer BaseBoardProduct SystemProductName SystemFamily SystemManufacturer"
-set "data=SAMSUNG ELECTRONICS CO., LTD. NP960XFH-XA2UK NP960XFH-XA2UK Galaxy Book3 Ultra SAMSUNG ELECTRONICS CO., LTD."
-
-for %%a in (%values%) do (
-    for /f "tokens=1,* delims= " %%b in ('echo %data%') do (
-        reg add "%key%" /v %%a /t REG_SZ /d "%%b" /f
-        set "data=%%c"
-    )
-)
+reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v BaseBoardManufacturer /t REG_SZ /d "SAMSUNG ELECTRONICS CO., LTD." /f
+reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v BaseBoardProduct /t REG_SZ /d "NP960XFH-XA2UK" /f
+reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemProductName /t REG_SZ /d "NP960XFH-XA2UK" /f
+reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemFamily /t REG_SZ /d "Galaxy Book3 Ultra" /f
+reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemManufacturer /t REG_SZ /d "SAMSUNG ELECTRONICS CO., LTD." /f
